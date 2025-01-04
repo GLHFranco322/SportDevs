a90a2c83a503b0a1c66d1d176bbae50ef16b30b5
Franco Spiazzi <francospiazzi96@gmail.com>
2025-01-03 23:05:40 -0300

    Realicé las rutas de carrito y coloqué un ejemplo de como se verá la sección final del pago

--- eventos/app.js
+++ eventos/app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productosRouter = require('./routes/productos');
 var registerRouter = require('./routes/register');
+let payRouter = require('./routes/pay');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/productos', productosRouter);
 app.use('/register', registerRouter);
+app.use('/pay', payRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
