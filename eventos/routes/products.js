ff6b284f019bb28d1f1dbf7d078aa6f21b593341
Franco Spiazzi <francospiazzi96@gmail.com>
2025-01-11 19:14:23 -0300

    ...

--- eventos/routes/products.js
+++ eventos/routes/products.js
@@ -1,11 +1,10 @@
-// Dentro de routes/products.js
 var express = require('express');
 var router = express.Router();
 var path = require('path');
 var fs = require('fs');
-var productsData = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/products.json'), 'utf-8'));
 
 router.get('/', function(req, res, next) {
+    var productsData = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/products.json'), 'utf-8'));
     res.render('products', { productos: productsData, title: 'Productos' });
 });
 
