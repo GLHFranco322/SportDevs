9922c0f2cec87d9736eac85d4999a0d424d3f3b2
Franco Spiazzi <francospiazzi96@gmail.com>
2025-05-03 00:14:09 -0300

    d

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -8,12 +8,12 @@ const { readJson, saveJson } = require('../db/index.js');
 const productsController = {
     index: (req, res) => {
         const products = readJson('products.json');
-        res.render('products', { title: 'Products', products, toThousand, categories });
+        res.render('user-views/products', { title: 'Products', products, toThousand, categories });
     },
     detail: (req, res) => {
         const products = readJson('products.json');
         const product = products.find(product => product.id === +req.params.id);
-        res.render('productDetail', { title: 'Product Detail', product, products, toThousand });
+        res.render('user-views/productDetail', { title: 'Product Detail', product, products, toThousand });
     },
     getProductById: (req, res, next) => {
         const products = readJson('products.json');
@@ -28,7 +28,7 @@ const productsController = {
     search: (req, res) => {
         const products = readJson('products.json');
         const query = req.query.q.toLowerCase();
-        const filteredProducts = products.filter(product => 
+        const filteredProducts = products.filter(product =>
             product.name.toLowerCase().includes(query) ||
             product.description.toLowerCase().includes(query)
         );
